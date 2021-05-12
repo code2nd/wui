b1f2f52855e128eb6a39fb11c837e6c6ed117112
Jalamy <wy20150905zc@163.com>
2021-05-12 16:48:44 +0800

    style: 文件整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from 'components/Button';
+import Button, { ButtonType, ButtonSize } from 'components/Button/button';
+import Menu from 'components/Menu/menu';
+import MenuItem from 'components/Menu/menuItem';
 
 function App() {
   return (
@@ -8,13 +10,31 @@ function App() {
       <Button disabled>desabled button</Button>
       <Button btnType={ButtonType.Primary}>primary button</Button>
       <Button btnType={ButtonType.Danger}>danger button</Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+      <Button
+        btnType={ButtonType.Link}
+        href="http://www.baidu.com"
+        target="_blank"
+      >
         link button
       </Button>
       <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>
         disabled link button
       </Button>
       <Button size={ButtonSize.Large}>large button</Button>
+
+      <Menu
+        mode="vertical"
+        defaultIndex={0}
+        onSelect={(index) => {
+          console.log(index);
+        }}
+      >
+        <MenuItem index={0}>active</MenuItem>
+        <MenuItem index={1} disabled>
+          disabled
+        </MenuItem>
+        <MenuItem index={2}>xyz</MenuItem>
+      </Menu>
     </div>
   );
 }
